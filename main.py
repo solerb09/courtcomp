c48dfb66317fec2252f40fccb9c64710e00033f4
solerb09 <46010243+solerb09@users.noreply.github.com>
2024-11-22 15:01:59 -0500

    merged both csv into one dataframe with salary

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ import pandas as pd
 stats = pd.read_csv('2023_nba_player_stats.csv')
 salaries = pd.read_csv('Nba Player Salaries.csv')
 
+salaries.rename(columns={'Player Name': 'PName'}, inplace=True)
+print(salaries.columns)  # Check columns in stats DataFrame
 
-print(stats.head())
-print(salaries.head())
+
+# Select only the columns for player name and the 2022/2023 season
+salaries_2022_2023 = salaries[["PName","2022/2023"]]
+
+
+
+# Merge the filtered salaries with the stats DataFrame
+merged_df = pd.merge(stats, salaries_2022_2023, on="PName", how="inner")
+merged_df.rename(columns={'2022/2023': 'Salary'}, inplace=True)
+
+print(merged_df.head())
+
+
+
+
