64a0bd1538f06de7c16f51cbb6b73ce96f93cdda
solerb09 <46010243+solerb09@users.noreply.github.com>
2024-11-22 15:06:32 -0500

    used regex to convert salary to int

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ salaries_2022_2023 = salaries[["PName","2022/2023"]]
 
 # Merge the filtered salaries with the stats DataFrame
 merged_df = pd.merge(stats, salaries_2022_2023, on="PName", how="inner")
+
+
 merged_df.rename(columns={'2022/2023': 'Salary'}, inplace=True)
+
+merged_df["Salary"] = (
+    merged_df["Salary"]
+    .str.replace("$", "", regex=False)  # Remove the dollar sign
+    .str.replace(",", "", regex=False)  # Remove commas
+    .astype(int)  # Convert to integer
+)
 
 print(merged_df.head())
 
