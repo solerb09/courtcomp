698f596de3eb532d271659863b725de7b2a51725
Quin <quindog22@gmail.com>
2024-11-22 15:33:14 -0500

    added player salaries

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 import pandas as pd
 
-stats = pd.read_csv('2023_nba_player_stats.csv')
+# stats = pd.read_csv('2023_nba_player_stats.csv')
+# salaries = pd.read_csv('Nba Player Salaries.csv')
+
+# salaries.rename(columns={'Player Name': 'PName'}, inplace=True)
+# print(salaries.columns)  # Check columns in stats DataFrame
+
+
+# # Select only the columns for player name and the 2022/2023 season
+# salaries_2022_2023 = salaries[["PName","2022/2023"]]
+
+
+# # Merge the filtered salaries with the stats DataFrame
+# merged_df = pd.merge(stats, salaries_2022_2023, on="PName", how="inner")
+
+
+# merged_df.rename(columns={'2022/2023': 'Salary'}, inplace=True)
+
+# merged_df["Salary"] = (
+#     merged_df["Salary"]
+#     .str.replace("$", "", regex=False)  # Remove the dollar sign
+#     .str.replace(",", "", regex=False)  # Remove commas
+#     .astype(int)  # Convert to integer
+# )
+
+# print(merged_df.head())
+
+
+
+
+stats = pd.read_csv('2023_nba_player_stats_with_advanced.csv')
 salaries = pd.read_csv('Nba Player Salaries.csv')
 
-salaries.rename(columns={'Player Name': 'PName'}, inplace=True)
+salaries.rename(columns={'Player Name': 'Player'}, inplace=True)
 print(salaries.columns)  # Check columns in stats DataFrame
 
 
-# Select only the columns for player name and the 2022/2023 season
-salaries_2022_2023 = salaries[["PName","2022/2023"]]
+#Select only the columns for player name and the 2022/2023 season
+salaries_2022_2023 = salaries[["Player","2022/2023"]]
 
 
 
-# Merge the filtered salaries with the stats DataFrame
-merged_df = pd.merge(stats, salaries_2022_2023, on="PName", how="inner")
+#Merge the filtered salaries with the stats DataFrame
+merged_df = pd.merge(stats, salaries_2022_2023, on="Player", how="inner")
 
 
 merged_df.rename(columns={'2022/2023': 'Salary'}, inplace=True)
@@ -25,8 +54,8 @@ merged_df["Salary"] = (
     .astype(int)  # Convert to integer
 )
 
+merged_df.dropna(inplace=True)  # Drop rows with missing values
+
+
+
 print(merged_df.head())
-
-
-
-
